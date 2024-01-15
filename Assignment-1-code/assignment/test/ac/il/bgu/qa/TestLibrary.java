23131fea7cefdd358ccfb09c41dc738a4dd6f4c1
DenisPn <panor@post.bgu.ac.il>
2024-01-15 16:10:35 +0200

    Init

---
+++ Assignment-1-code/assignment/test/ac/il/bgu/qa/TestLibrary.java
@@ -0,0 +1,12 @@
+package ac.il.bgu.qa;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.mockito.*;
+
+public class TestLibrary {
+    @Test
+    public void test(){
+
+    }
+}
